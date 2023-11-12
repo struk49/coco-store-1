c845495ed47bf81ea63973ba3095add90b6c085f
struk49 <andrewstruk@yahoo.co.uk>
2023-11-12 13:26:35 +0000

    added product to db

--- shop/models.py
+++ shop/models.py
@@ -11,3 +11,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=254)
+    description = models.TextField(blank=True, null=True)
+    
+
+
+    def __str__(self):
+        return self.title
